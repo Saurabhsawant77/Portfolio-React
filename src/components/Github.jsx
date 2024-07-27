b62893ad72143fe1c0ee87e83b07b738e235e0a2
Saurabh Sawant <113126470+Saurabhsawant77@users.noreply.github.com>
2024-07-27 13:42:23 +0530

    Github Done

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -26,19 +26,19 @@ const Github = () => {
   }, []);
 
   return (
-    <div className="main-githubContainer bg-slate-200 space-y-4">
-      <h1 className="text-center text-3xl pt-4">Github Repositories</h1>
-      {error && <p>{error}</p>}
+    <div className="main-githubContainer bg-slate-200 p-4">
+      <h1 className="text-center text-3xl mb-4">Github Repositories</h1>
+      {error && <p className="text-center text-red-500">{error}</p>}
 
-    <div className="flex w-full p-4 m-4 flex-wrap">
+      <div className="flex flex-wrap justify-center gap-4">
         {repos.map((repo, index) => (
-          <div key={index} className="bg-slate-400 shadow-lg rounded-lg hover:bg-gray-500 hover:text-white w-full m-4 px-3 py-3 items-center ">
+          <div key={index} className="bg-slate-400 shadow-lg rounded-lg h-2/3 hover:bg-gray-500 hover:text-white p-4 w-full sm:w-80 md:w-96 lg:w-1/4">
             <a href={urls[index]} target="_blank" rel="noopener noreferrer">
-             <p> {repo} </p>
+              <p className="text-center">{repo}</p>
             </a>
           </div>
         ))}
-    </div>
+      </div>
     </div>
   );
 };
